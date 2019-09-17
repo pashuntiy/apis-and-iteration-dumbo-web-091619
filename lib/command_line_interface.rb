3467008d0ef670b41230058a9772a56967204c02
Pavel Ilin <pavel_ilin@yahoo.com>
2019-09-17 18:11:05 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hello my dear Geek"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.chomp
 end
